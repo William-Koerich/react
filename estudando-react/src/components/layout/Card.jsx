5db7033a9793da95e07aa5eb4375410b48782e5c
William Koerich <william.koerich@fw7.com.br>
2021-01-06 19:01:41 -0300

    Added type checking.

--- estudando-react/src/components/layout/Card.jsx
+++ estudando-react/src/components/layout/Card.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './Card.css'
 
-export default props => (
-  <div className="Card">
-    <div className="Icone" style={{ backgroundColor: props.cor }}>
+const Card = props => (
+  <div className="card">
+    <div className="icone" style={{ backgroundColor: props.cor }}>
       {props.icon}
     </div>
-    <div className="Titulo">{props.titulo || 'Título'}</div>
+    <div className="titulo">{props.titulo || 'Título'}</div>
 
-    <div className="Subtitulo">{props.subtitulo || 'Subtitulo'}</div>
+    <div className="subtitulo">{props.subtitulo || 'Subtitulo'}</div>
 
-    <div className="Descricao">{props.descricao || 'Descrição'}</div>
+    <div className="descricao">{props.descricao || 'Descrição'}</div>
 
     <div className="hr"></div>
 
-    <div className="MaisInformacoes">+ Informações</div>
+    <div className="mais-informacoes">+ Informações</div>
   </div>
 )
+export default Card
+
+Card.propTypes = {
+  cor: PropTypes.string,
+  titulo: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  subtitulo: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+}
