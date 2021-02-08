357ab208d0ea5d80a27c77e390556259553ec8b9
Anderson <and.espindola@hotmail.com>
2021-02-08 19:24:13 -0300

    Added discount total.

--- loja/src/App.tsx
+++ loja/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, KeyboardEvent } from 'react'
 import axios from 'axios'
 import Cart from './Cart/Cart'
 import Store from './Store/Store'
@@ -22,6 +22,16 @@ const App = () => {
   const [productsAdded, setProductsAdded] = useState<Product[]>([])
 
   /**
+   * Total.
+   */
+  const [totalValue, setTotalValue] = useState<Number>(0)
+
+  /**
+   * Discount percentage.
+   */
+  const [discount, setDiscount] = useState<Number>(0)
+
+  /**
    * Conection for DB.
    */
   useEffect(() => {
@@ -36,8 +46,8 @@ const App = () => {
 
   function post() {
     const order = {
-      totalValue: calculateTotal(productsAdded),
-      discountTotal: 0
+      totalValue: totalValue,
+      discountTotal: discount
     }
 
     axios
@@ -51,23 +61,29 @@ const App = () => {
   function addItemInCart(product: Product) {
     const productsAddedInTheCart = [...productsAdded, product]
     setProductsAdded(productsAddedInTheCart)
+    calculateTotal(productsAddedInTheCart)
   }
 
   /**
    * Function used for calculate total.
    */
   function calculateTotal(productsAddedInTheCart: Array<Product>) {
-    return productsAddedInTheCart.reduce(
+    const cartValue = productsAddedInTheCart.reduce(
       (value: number, product: Product) => value + product.price,
       0
     )
+    return setTotalValue(cartValue)
   }
 
   /**
    * Remove itens in cart.
    */
   const removeItemInCart = (idx: number) => {
-    setProductsAdded([...productsAdded].filter((_, index) => index !== idx))
+    const productsAddedInTheCart = [...productsAdded].filter(
+      (_, index) => index !== idx
+    )
+    setProductsAdded(productsAddedInTheCart)
+    calculateTotal(productsAddedInTheCart)
   }
 
   /**
@@ -87,12 +103,20 @@ const App = () => {
   }
 
   const finalizeOrder = () => {
+    post()
     setProductsAdded([])
-    saveOrder()
+    setTotalValue(0)
+    setDiscount(0)
   }
 
-  const saveOrder = () => {
-    post()
+  const applyDiscount = () => {
+    const calculateDiscount = (+totalValue * +discount) / 100
+    const total = +totalValue - calculateDiscount
+    setTotalValue(total)
+  }
+
+  const setDiscountValue = (event: KeyboardEvent<HTMLInputElement>) => {
+    setDiscount(+event.currentTarget.value)
   }
 
   return (
@@ -103,9 +127,13 @@ const App = () => {
         filterProducts={filterProducts}
       />
       <Cart products={productsAdded} onRemove={removeItemInCart} />
+      <br />
+      <label>Desconto: </label>
+      <input type="text" onKeyUp={setDiscountValue.bind(this)} />
+      <button onClick={applyDiscount}>Aplicar</button>
 
       <h3>Total</h3>
-      <h4>R$ {calculateTotal(productsAdded)}</h4>
+      <h4>R$ {totalValue}</h4>
 
       <button onClick={finalizeOrder}>Finalizar</button>
     </div>
